c9e69aee5e27a416140260fb1060d8b7c16706f7
ruggero <ruggero.rabitti@visualengines.com>
2022-05-24 12:20:49 +0200

    implementata Dependency Injection funzionante per i 4 controller (Spring instanzia 4 beans: 1 per ogni controller più uno per il service da loro utilizzato)

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectedController;
+import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -17,6 +20,27 @@ public class SfgDiApplication {
 		String greeting = myControlller.sayHello();
 
 		System.out.println(greeting);
+
+		// Property based Dependency Injection
+		System.out.println("\n---------Property D.I.");
+
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+
+		System.out.println(propertyInjectedController.getGreeting());
+
+		// Setter based Dependency Injection
+		System.out.println("\n----------Setter D.I.");
+
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+
+		System.out.println(setterInjectedController.getGreeting());
+
+		// Constructor based Dependency Injection
+		System.out.println("\n----------Constructor D.I.");
+
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 
 }
